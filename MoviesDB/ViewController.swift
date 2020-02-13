5b12871a2a6996c551577e98351163ac75d34d70
kimbumlak <kimbumlak@gmail.com>
2020-02-13 18:13:42 -0500

    Initial Commit

---
+++ MoviesDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesDB
+//
+//  Created by BumLak Kim on 2/13/20.
+//  Copyright © 2020 BumLak Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
