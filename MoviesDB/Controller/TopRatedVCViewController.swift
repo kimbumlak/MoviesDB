987f955628b2d3da009d8bef12e22fbbc47e20b9
kimbumlak <kimbumlak@gmail.com>
2020-02-16 21:40:06 -0500

    Implemented TopRated Controller

---
+++ MoviesDB/Controller/TopRatedVCViewController.swift
@@ -0,0 +1,59 @@
+//
+//  TopRatedVCViewController.swift
+//  MoviesDB
+//
+//  Created by BumLak Kim on 2/16/20.
+//  Copyright © 2020 BumLak Kim. All rights reserved.
+//
+
+import UIKit
+
+class TopRatedVCViewController: UIViewController {
+    @IBOutlet private weak var collectionView: UICollectionView!
+    private var movies: [Movie]?
+    private var page: Int = 1
+    private var totalPages: Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let width = (view.frame.size.width - 20) / 3
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: 200)
+        fetch()
+    }
+    private func fetch(_ page: Int = 1) {
+        API.fetchPopularMovies(page) { data in
+            self.totalPages = data.totalPages
+            self.movies = data.results
+            self.collectionView.reloadData()
+        }
+    }
+    private func loadMoreData() {
+        if page < totalPages {
+            page += 1
+            OperationQueue.main.addOperation {
+                API.fetchPopularMovies(self.page) { data in
+                    self.movies? += data.results
+                    self.collectionView.reloadData()
+                }
+            }
+        }
+    }
+}
+
+extension TopRatedVCViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies?.count ?? 0
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TopRated", for: indexPath) as! MovieCollectionViewCell
+        cell.movie = movies?[indexPath.item]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath){
+        guard let count = movies?.count else {fatalError()}
+        if indexPath.item == count - 1 {
+            self.loadMoreData()
+        }
+    }
+}
